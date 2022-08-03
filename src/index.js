daca702454c3c368f9d4a4b1f2245c2f1e520e48
mkravchenko-2022 <mkravchenko@xmentium.com>
2022-08-03 15:51:01 -0600

    Completed

--- src/index.js
+++ src/index.js
@@ -3,7 +3,13 @@
  * true, если есть различие, false. То есть проверяет каждое свойство, вне зависимости от вложенности,
  * делаем через рекурсию(а других вариантов и нет)
  */
-export const deepEqual = (obj, anotherObject) => {};
+export const deepEqual = (obj, anotherObject) => {
+  return Object.entries(obj).every(([key, value]) => {
+    return typeof value !== 'object'
+      ? value === anotherObject[key]
+      : deepEqual(value, anotherObject[key]);
+  });
+};
 
 /**
  * Принимает объект, возвращает его глубокую копию, то есть ни одно свойство
@@ -12,10 +18,25 @@ export const deepEqual = (obj, anotherObject) => {};
  * то тогда в рекурсию. С объектом также. Поскольку массив при typeof возвращает object, чтобы
  * их различить берем метод Array.isArray и он на массивах вернет тру
  */
-export const deepCopy = (obj) => {};
+export const deepCopy = (obj) => {
+  if (typeof obj !== 'object') return obj;
+  else if (Array.isArray(obj)) return obj.map(deepCopy);
+  return Object.fromEntries(
+    Object.entries(obj).map(([key, value]) => [key, deepCopy(value)])
+  );
+};
 
 /**
  * Мы передаем объект, и должны вернуть массив уникальных названий свойств
  * То есть если у нас объект { name: { bohdan: { name: 'test' } } } вернет ['name', 'bohdan']
  */
-export const getAllObjectKeys = (obj) => {};
+export const getAllObjectKeys = (obj) => {
+  let set = new Set(
+    Object.entries(obj).reduce((accum, [key, value]) => {
+      return typeof value !== 'object'
+        ? [...accum, key]
+        : [...accum, key, ...getAllObjectKeys(value)];
+    }, [])
+  );
+  return Array.from(set);
+};
